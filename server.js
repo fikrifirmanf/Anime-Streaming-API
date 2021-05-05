a34c7a2ebe1c245d680f03ab1789a0ccfeba4a2c
Fikri Firman Fadilah <fikrifirmanfadilah@gmail.com>
2021-05-05 09:36:12 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const ScrapeController = require('./controllers/scrape.controller')
+
+app.get('/category',ScrapeController.onGoing)
+app.get('/detail',ScrapeController.animeDetail)
+app.get('/anime',ScrapeController.anime)
+app.get('/search',ScrapeController.searchAnime)
+app.get('*', (req,res)=>{
+    res.send('<h2>Muaach</h2>')
+})
+
+app.listen(PORT, ()=>{
+    console.log('Server connected on port'+PORT)
+})
