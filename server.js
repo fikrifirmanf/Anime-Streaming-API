6017b54237393fdf871433953b86140f816f3a74
Fikri Firman Fadilah <fikrifirmanfadilah@gmail.com>
2021-05-06 14:01:14 +0700

    update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8766
 const ScrapeController = require('./controllers/scrape.controller')
 
 app.get('/category',ScrapeController.onGoing)
