4b44ba11ac5ca82593770bf2eafd5fffdeab9907
Fikri Firman Fadilah <fikrifirmanfadilah@gmail.com>
2021-05-06 14:12:13 +0700

    aosjd

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ app.get('/category',ScrapeController.onGoing)
 app.get('/detail',ScrapeController.animeDetail)
 app.get('/anime',ScrapeController.anime)
 app.get('/search',ScrapeController.searchAnime)
-app.get('*', (req,res)=>{
-    res.send('<h2>Muaach</h2>')
-})
+// app.get('*', (req,res)=>{
+//     res.send('<h2>Muaach</h2>')
+// })
 
 app.listen(PORT, ()=>{
     console.log('Server connected on port'+PORT)
