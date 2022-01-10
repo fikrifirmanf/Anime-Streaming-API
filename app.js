5a635c175ed96e53422d48ba7793f173c215dde6
rii amri <riiamri23@gmail.com>
2022-01-10 19:51:22 +0700

    my commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const helmet = require('helmet')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 const ScrapeController = require('./controllers/scrape.controller')
 
 app.use(cors())
